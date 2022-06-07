89cb627ac2c42b98e48646b517f9d575d9942246
Biff <Biff@blind-spot>
2022-06-07 12:20:04 +0300

    i will install garden programs :Melvin-Odoom but a man who chooses a bush over a bench i cant trust :James-Acaster it's good it didn't happen to Melvin - otherwise he'd be on a bench wearing a dress and nothing else

--- program.sh
+++ program.sh
@@ -1,103 +1,62 @@
 #!/bin/bash
 
-cp_vscode_settings(){
-  cp -f ./src/Biff/settings.json ~/.config/Code/User/settings.json
+
+install(){
+  npm i --no-package-lock
 }
 
-cp_gitconfig(){
-  cp -f ./src/Biff/.gitconfig ~/.gitconfig
+copy(){
+  mkdir -p out/jar/ui/
+  mkdir -p out/jar/src/Biff/
+  cp src/Biff/index.html out/jar/ui/index.html
+  cp src/Biff/style.css out/jar/ui/style.css
 }
 
-git_user(){
-  git config --global user.name "$1"
-  git config --global user.email "$2"
+shadow(){
+  clj -A:shadow:ui -M -m shadow.cljs.devtools.cli "$@"
 }
 
-install_libsecret(){
-  sudo apt-get install libsecret-1-0 libsecret-1-dev
-  cd /usr/share/doc/git/contrib/credential/libsecret
-  sudo make
-  git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
+repl(){
+  install
+  copy
+  shadow clj-repl
+  # (shadow/watch :main)
+  # (shadow/watch :ui)
+  # (shadow/repl :main)
+  # :repl/quit
 }
 
-install_clj(){
-  SCRIPT_NAME=linux-install-1.10.3.855.sh
-  FILE=/tmp/${SCRIPT_NAME}
-  sudo apt update
-  sudo apt install -y rlwrap
-  curl -o /tmp/${SCRIPT_NAME} https://download.clojure.org/install/${SCRIPT_NAME}
-  chmod +x ${FILE}
-  sudo bash ${FILE}
-  sudo rm ${FILE}
+compile(){
+  shadow release :ui
 }
 
-install_gnome_shell_clock_override(){
-  cd ../
-  git clone https://github.com/stuartlangridge/gnome-shell-clock-override
-  cd gnome-shell-clock-override
-  make -j install
+release(){
+  rm -rf out
+  install
+  copy
+  compile
 }
 
-install_jdk(){
-  # 11.0.2
-  # 14.0.2
-  # 16.0.1
-  FILE=openjdk-$1_linux-x64_bin.tar.gz
-  URL=https://download.java.net/java/GA/jdk11/9/GPL/$FILE
-  echo $URL
-  wget $URL -P ~/Downloads
-  mkdir -p ~/bin
-  tar -xvzf ~/Downloads/$FILE -C ~/bin
+tag(){
+  COMMIT_HASH=$(git rev-parse --short HEAD)
+  COMMIT_COUNT=$(git rev-list --count HEAD)
+  TAG="$COMMIT_COUNT-$COMMIT_HASH"
+  git tag $TAG $COMMIT_HASH
+  echo $COMMIT_HASH
+  echo $TAG
 }
 
-bashrc(){
-  cat ./src/Biff/.bashrc >> ~/.bashrc
-}
-
-install_golang(){
-  FILENAME=go1.15.7.linux-amd64.tar.gz
-  wget https://go.dev/dl/$FILENAME -P ~/Downloads
-  mkdir -p ~/bin
-  tar -xvzf ~/Downloads/$FILENAME -C ~/bin
-  go version
-}
-
-install_qt(){
-  sudo apt-get install qt5-default
-}
-
-install_lein(){
-  wget https://raw.githubusercontent.com/technomancy/leiningen/2.9.5/bin/lein -P ~/Downloads
-  mkdir -p ~/bin
-  mv ~/Downloads/lein ~/bin
-  chmod a+x ~/bin/lein
-}
-
-install_nodejs(){
-  curl -fsSL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-  sudo apt-get install -y nodejs
-}
-
-install_mono(){
-  sudo apt install -y gnupg ca-certificates
-  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-  echo "deb https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
-  sudo apt update
-  sudo apt install -y mono-complete
-}
-
-install_scheme(){
-  # sudo apt-get install -y build-essential  uuid-dev libncurses5-dev libncursesw5-dev libx11-dev
-  # sudo apt-get install -y lsb-release build-essential libssl-dev python
-
-  # cd ~/bin && git clone https://github.com/cisco/ChezScheme
-
-  # cd ~/bin/ChezScheme && \
-  # git checkout 5aba39c40b46cee61a388d71ef4e5eebfa717108 && \
-  #   ./configure && \
-  #   sudo make install
-
-  sudo apt install chezscheme
+gh_pages(){
+  ORIGIN=$(git config --get remote.origin.url)
+  cd out/jar/ui
+  git init -b gh-pages
+  git remote add origin $ORIGIN
+  git config --local include.path "../../../../.gitconfig"
+  git add .
+  git commit -m "and where's my reports?"
+  git push -f origin gh-pages
+  rm -rf .git
+  cd ../../../
 }
 
 "$@"
---
+++ src/Biff/ui_main.cljs
@@ -0,0 +1,121 @@
+(ns Biff.ui-main
+  (:require
+   [clojure.core.async :as a
+    :refer [chan put! take! close! offer! to-chan! timeout
+            sliding-buffer dropping-buffer
+            go >! <! alt! alts! do-alts
+            mult tap untap pub sub unsub mix unmix admix
+            pipe pipeline pipeline-async]]
+   [clojure.string]
+   [clojure.pprint]
+   [cljs.core.async.impl.protocols :refer [closed?]]
+   [cljs.core.async.interop :refer-macros [<p!]]
+   [goog.string.format]
+   [goog.string :refer [format]]
+   [goog.object]
+   [cljs.reader :refer [read-string]]
+   [goog.events]
+
+   ["react" :as react]
+   ["react-dom/client" :as react-dom.client]
+
+   [reagent.core]
+   [reagent.dom]
+
+   ["antd/lib/layout" :default AntdLayout]
+   ["antd/lib/menu" :default AntdMenu]
+   ["antd/lib/button" :default AntdButton]
+   ["antd/lib/row" :default AntdRow]
+   ["antd/lib/col" :default AntdCol]
+   ["antd/lib/input" :default AntdInput]
+   ["antd/lib/table" :default AntdTable]
+   ["antd/lib/tabs" :default AntdTabs]
+
+   ["konva/lib/shapes/Rect"]
+   ["konva" :default Konva]
+   ["react-konva" :as ReactKonva :rename {Stage KonvaStage
+                                          Layer KonvaLayer
+                                          Rect KonvaRect
+                                          Path KonvaPath
+                                          Circle KonvaCircle
+                                          Group KonvaGroup
+                                          Wedge KonvaWedge
+                                          RegularPolygon KonvaRegularPolygon}]
+
+   ["@react-spring/web" :as ReactSpring :rename {animated ReactSpringAnimated
+                                                 Spring ReactSpringSpring}]
+
+   [Biff.ui-seed :refer [root]]))
+
+(defn rc-ui
+  []
+  (reagent.core/with-let
+    [dataA (reagent.core/cursor (:stateA root) [:simple-double-full])]
+    [:> (.-Content AntdLayout)
+     {:style {:background-color "white"}}
+     [:<>
+      [:div {}
+       [:div "who's gonna pay my cleaning bill?"]]
+      [:> AntdRow
+       [:> AntdTable
+        {:size "small"
+         :style {:width "100%"
+                 :height "80%"}
+         :rowKey :name
+         :columns [{:title "name"
+                    :dataIndex "name"
+                    :key "name"}
+                   {:title "git-url"
+                    :dataIndex "git-url"
+                    :key "git-url"}]
+         :dataSource [{:name "Cara-Dune" :git-url "https://github.com/move-me-to-ipfs-shipyard/Cara-Dune"}
+                      {:name "Dr-Pershing" :git-url "https://github.com/move-me-to-ipfs-shipyard/Dr-Pershing"}]}]]]]))
+
+(defmulti op :op)
+
+(defmethod op :ping
+  [value]
+  (go
+    (clojure.pprint/pprint value)
+    (put! (:program-send| root) {:op :pong
+                                 :from :ui
+                                 :moneybuster :Jesus})))
+
+(defmethod op :pong
+  [value]
+  (go
+    (clojure.pprint/pprint value)))
+
+(defn ops-process
+  [{:keys []
+    :as opts}]
+  (go
+    (loop []
+      (when-let [value (<! (:ops| root))]
+        (<! (op value))
+        (recur)))))
+
+(defn -main
+  []
+  (go
+    #_(<! (timeout 1000))
+    (println "twelve is the new twony")
+    (println ":Madison you though i was a zombie?")
+    (println ":Columbus yeah, of course - a zombie")
+    (println ":Madison oh my God, no - i dont even eat meat - i'm a vegatarian - vegan actually")
+    #_(set! (.-innerHTML (.getElementById js/document "ui"))
+            ":Co-Pilot i saw your planet destroyed - i was on the Death Star :_ which one?")
+    (ops-process {})
+    (.render @(:dom-rootA root) (reagent.core/as-element [rc-ui]))
+    #_(websocket-process {:send| (:program-send| root)
+                        :recv| (:ops| root)})))
+
+(defn reload
+  []
+  (when-let [dom-root @(:dom-rootA root)]
+    (.unmount dom-root)
+    (let [new-dom-root (react-dom.client/createRoot (.getElementById js/document "ui"))]
+      (reset! (:dom-rootA root) new-dom-root)
+      (.render @(:dom-rootA root) (reagent.core/as-element [rc-ui])))))
+
+#_(-main)
